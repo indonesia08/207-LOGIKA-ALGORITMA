d7a05d3954f1b593214e9197dd556e098eabbbf6
Yusuf <40708511+indonesia08@users.noreply.github.com>
2018-09-21 00:22:47 +0700

    Operasi Matematika

---
+++ Luas_Segitiga.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <CONSTREA.H>
+void main()
+{
+int alas, tinggi , luas;
+cout << "Masukan alas : ";
+cin >> alas;
+cout << "Masukan tinggi : ";
+cin >> tinggi;
+luas = (alas*tinggi)/2;
+cout <<" luas segitiga adalah : "<<luas<<endl;
+getch();
+}
