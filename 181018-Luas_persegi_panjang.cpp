ae17d590a7206cbe2e188f3df8073a1f9f5795ba
Yusuf <40708511+indonesia08@users.noreply.github.com>
2018-12-12 22:59:59 +0700

    Create 181018-Luas_persegi_panjang.cpp

---
+++ 181018-Luas_persegi_panjang.cpp
@@ -0,0 +1,22 @@
+/*
+Matakuliah    				:207-LOGIKA-ALGORITMA
+Tanggal       				:18 Oktober 2018
+*/
+#include <constrea.h>
+void main()
+{
+int panjang,lebar,luas;
+clrscr();
+cout<<"==================================="<<endl;
+cout<<"  Menghitung Luas Persegi Panjang"<<endl;
+cout<<"==================================="<<endl;
+cout<<endl;
+cout<<"Masukkan Panjang            : ";
+cin>>panjang;
+cout<<"Masukkan Lebar              : ";
+cin>>lebar;
+luas = (panjang*lebar);
+cout<<endl;
+cout <<"Luas Persegi Panjang Adalah : "<<luas<<endl;
+getch();
+}
