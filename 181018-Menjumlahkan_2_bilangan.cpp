d4b6530e0ac9b70f50f61ce5007e41182f247311
Yusuf <40708511+indonesia08@users.noreply.github.com>
2018-12-12 22:54:51 +0700

    Create 181018-Menjumlahkan_2_bilangan.cpp

---
+++ 181018-Menjumlahkan_2_bilangan.cpp
@@ -0,0 +1,23 @@
+/*
+Matakuliah    				:207-LOGIKA-ALGORITMA
+Tanggal       				:18 Oktober 2018
+*/
+#include <constrea.h>
+void main()
+{
+int bil1,bil2,tambah;
+
+clrscr();
+cout<<"============================="<<endl;
+cout<<"  Menjumlahkan Dua Bilangan"<<endl;
+cout<<"============================="<<endl;
+cout<<endl;
+cout<<"Masukan Bilangan 1       : ";
+cin>>bil1;
+cout<<"Masukan Bilangan 2       : ";
+cin>>bil2;
+tambah = bil1+bil2;
+cout<<endl;
+cout<<"Hasil penjumlahan adalah : "<<tambah<<endl;
+getch();
+}
